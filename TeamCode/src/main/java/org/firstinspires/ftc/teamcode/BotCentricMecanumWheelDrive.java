2a00656a8e8a82afd9db96b1c710572f987f89d5
C4 Patino <c4patino@gmail.com>
2022-11-14 16:15:59 -0600

    Added Opmode groups for driver-centric and bot-centric, refactored hardware mappings to botclass

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotCentricMecanumWheelDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotCentricMecanumWheelDrive.java
@@ -1,83 +1,51 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="Bot Centric Mecanum Drive", group="Linear Opmode")
-@Disabled
+import org.firstinspires.ftc.teamcode.botclasses.Athena;
+
+@TeleOp(name="Mecanum Drive", group="Bot Centric")
 public class BotCentricMecanumWheelDrive extends LinearOpMode {
-
-    // Declare OpMode members for each of the 4 motors.
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftFrontDrive = null;
-    private DcMotor leftBackDrive = null;
-    private DcMotor rightFrontDrive = null;
-    private DcMotor rightBackDrive = null;
+    // Declare the bot class instance
+    Athena robot = new Athena(hardwareMap);
 
     @Override
     public void runOpMode() {
-
-        // Initialize the hardware variables. The names are retrieved from the configure robot menu.
-        // The names in the code must match the names of the motors in the hardware mapping
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "leftFrontDrive");
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "leftBackDrive");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "rightBackDrive");
-
-        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
-
-        // Wait for the game to start (driver presses PLAY)
+        // Add ready status to telemetry
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
+        // Wait for game start, abort if canceled
         waitForStart();
-
         if (isStopRequested()) return;
 
-        runtime.reset();
+        // Reset runtime timer to 0
+        robot.runtime.reset();
 
-        // run until the end of the match (driver presses STOP)
+
         while (opModeIsActive()) {
-            double max;
-
             // Gather sample game pad input data
             double y = -gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x * 1.1; // Multiplied by 1.1 to counter imperfect strafing
             double yaw = gamepad1.right_stick_x;
 
-            // Calculate motor powers based off of game pad inputs
-            double leftFrontPower  = y + x + yaw;
-            double rightFrontPower = y - x - yaw;
-            double leftBackPower   = y - x + yaw;
-            double rightBackPower  = y + x - yaw;
-
             // Normalize the values so no wheel power exceeds 100%
-            // This ensures that the robot maintains the desired motion.
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-            max = Math.max(max, Math.abs(leftBackPower));
-            max = Math.max(max, Math.abs(rightBackPower));
+            // This also ensures that the power ratios are preserved
+            double max = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(yaw), 1);
+            double leftFrontPower  = (y + x + yaw) / max;
+            double rightFrontPower = (y - x - yaw) / max;
+            double leftBackPower   = (y - x + yaw) / max;
+            double rightBackPower  = (y + x - yaw) / max;
 
-            if (max > 1.0) {
-                leftFrontPower  /= max;
-                rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
-            }
+            // Set the wheel power to calculated values
+            robot.leftFrontDrive.setPower(leftFrontPower);
+            robot.rightFrontDrive.setPower(rightFrontPower);
+            robot.leftBackDrive.setPower(leftBackPower);
+            robot.rightBackDrive.setPower(rightBackPower);
 
-            // Send calculated power to wheels
-            leftFrontDrive.setPower(leftFrontPower);
-            rightFrontDrive.setPower(rightFrontPower);
-            leftBackDrive.setPower(leftBackPower);
-            rightBackDrive.setPower(rightBackPower);
-
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            // Show the elapsed game time and wheel power in telemetry
+            telemetry.addData("Runtime", robot.runtime.toString());
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
             telemetry.update();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverCentricMecanumWheelDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverCentricMecanumWheelDrive.java
@@ -1,89 +1,62 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.botclasses.Athena;
 
-@TeleOp(name="Driver Centric Mecanum Drive", group="Linear Opmode")
-@Disabled
+@TeleOp(name="Mecanum Drive", group="Driver Centric")
 public class DriverCentricMecanumWheelDrive extends LinearOpMode {
-
-    // Declare OpMode members for each of the 4 motors.
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftFrontDrive = null;
-    private DcMotor leftBackDrive = null;
-    private DcMotor rightFrontDrive = null;
-    private DcMotor rightBackDrive = null;
+    // Declare the bot class instance
+    Athena robot = new Athena(hardwareMap);
 
     @Override
     public void runOpMode() {
-
-        // Initialize the hardware variables. Note that the strings used here must correspond
-        // to the names assigned during the robot configuration step on the DS or RC devices.
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "leftFrontDrive");
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "leftBackDrive");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "rightBackDrive");
-
-        // Keep modifying these values until ALL the wheels move the robot forward when
-        // you push the left joystick forward.
-        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
-
-        // Wait for the game to start (driver presses PLAY) and initialize telemetry display
+        // Add ready status to telemetry
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        // Retrieve the IMU from the hardware map
-        BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        // Technically this is the default, however specifying it is clearer
-        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
-        // Without this, data retrieving from the IMU throws an exception
-        imu.initialize(parameters);
-
+        // Wait for game start, abort if canceled
         waitForStart();
-
         if (isStopRequested()) return;
 
-        runtime.reset();
+        // Reset runtime timer to 0
+        robot.runtime.reset();
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
+            // Gather sample game pad input data
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x * 1.1; // Multiplied by 1.1 to counter imperfect strafing
+            double yaw = gamepad1.right_stick_x;
 
-            // Read inverse IMU heading, as the IMU heading is CW positive
-            double botHeading = -imu.getAngularOrientation().firstAngle;
+            // Reverse the IMU heading (to go counter-clockwise) and sample data
+            double botHeading = -robot.imu.getAngularOrientation().firstAngle;
 
-            // Calculate headings using trigonometric equations
+            // Calculate the amount of rotation needed on each axis to correct for angle
             double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
             double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
 
             // Normalize the values so no wheel power exceeds 100%
-            // This ensures that the robot maintains the desired motion.
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double leftFrontPower = (rotY + rotX + rx) / denominator;
-            double leftBackPower = (rotY - rotX + rx) / denominator;
-            double rightFrontPower = (rotY - rotX - rx) / denominator;
-            double rightBackPower = (rotY + rotX - rx) / denominator;
+            // This also ensures that the power ratios are preserved
+            double max = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(yaw), 1);
+            double leftFrontPower = (rotY + rotX + yaw) / max;
+            double leftBackPower = (rotY - rotX + yaw) / max;
+            double rightFrontPower = (rotY - rotX - yaw) / max;
+            double rightBackPower = (rotY + rotX - yaw) / max;
 
-            leftFrontDrive.setPower(leftFrontPower);
-            leftBackDrive.setPower(leftBackPower);
-            rightFrontDrive.setPower(rightFrontPower);
-            rightBackDrive.setPower(rightBackPower);
+            // Set the wheel power to calculated values
+            robot.leftFrontDrive.setPower(leftFrontPower);
+            robot.leftBackDrive.setPower(leftBackPower);
+            robot.rightFrontDrive.setPower(rightFrontPower);
+            robot.rightBackDrive.setPower(rightBackPower);
 
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
-            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+            // Show the elapsed game time and wheel power in telemetry
+            telemetry.addData("Runtime", robot.runtime.toString());
+            telemetry.addData("Front Left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
+            telemetry.addData("Back  Left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
             telemetry.update();
         }
     }}

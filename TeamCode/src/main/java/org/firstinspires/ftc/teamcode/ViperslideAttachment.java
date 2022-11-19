7cd07e0b6790ec0db109f7011f344c8e6d4c4161
C4 Patino <c4patino@gmail.com>
2022-11-18 23:58:34 -0600

    Programmed test viperslide code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ViperslideAttachment.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.drive.Viperslide;
+
+
+@TeleOp(name="Viperslide", group="Test")
+public class ViperslideAttachment extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        ElapsedTime runtime = new ElapsedTime();
+
+        // Declare the bot class instance
+        Viperslide viperslide = new Viperslide(hardwareMap);
+
+        // Add ready status to telemetry
+        telemetry.addData("Status", "<-- Press play to begin");
+        telemetry.update();
+
+        // Wait for game start, abort if canceled
+        waitForStart();
+        if (isStopRequested()) return;
+
+        // Reset runtime timer to 0
+        runtime.reset();
+        while (opModeIsActive()) {
+            double leftY = -gamepad1.left_stick_y;
+
+            if (leftY != 0) {
+                viperslide.sliderMotor.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+                if (gamepad1.left_bumper) {
+                    viperslide.sliderMotor.setPower(leftY / 2);
+                } else {
+                    viperslide.sliderMotor.setPower(leftY);
+                }
+            }
+
+            if (gamepad1.a) {
+                viperslide.sliderMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                viperslide.sliderMotor.setTargetPosition(viperslide.HIGH);
+                viperslide.sliderMotor.setPower(1);
+            }
+
+            if (gamepad1.b) {
+                viperslide.sliderMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                viperslide.sliderMotor.setTargetPosition(viperslide.MED);
+                viperslide.sliderMotor.setPower(1);
+            }
+
+            if (gamepad1.x) {
+                viperslide.sliderMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                viperslide.sliderMotor.setTargetPosition(viperslide.LOW);
+                viperslide.sliderMotor.setPower(1);
+            }
+
+            if (gamepad1.y) {
+                viperslide.sliderMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                viperslide.sliderMotor.setTargetPosition(viperslide.GROUND);
+                viperslide.sliderMotor.setPower(1);
+            }
+
+            // Show the elapsed game time and update arm position.
+            telemetry.addData("Runtime", runtime.toString());
+            telemetry.addData("Height", viperslide.sliderMotor.getCurrentPosition());
+            telemetry.update();
+        }
+    }}

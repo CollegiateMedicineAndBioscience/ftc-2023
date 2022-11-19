df07c38573576dc92da47ac15681e3c298a6d8e2
C4 Patino <c4patino@gmail.com>
2022-11-18 23:24:41 -0600

    Removed unnecesary import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveForward.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveForward.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.drive.AthenaMecanumDrive;
